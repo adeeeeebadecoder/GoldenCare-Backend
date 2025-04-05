6f06204fb6012d92bc0bc570fc1674ee94e05755
fahadi04 <fahadmasrur786@gmail.com>
2025-04-05 14:50:55 +0530

    modification in admin routes

--- routes/reminderRoutes.js
+++ routes/reminderRoutes.js
@@ -1,9 +1,43 @@
+// const express = require("express");
+// const { createReminder } = require("../controllers/reminderController");
+// const { protect } = require("../middlewares/authMiddleware");
+
+// const router = express.Router();
+
+// router.post("/create", protect, createReminder);
+
+// module.exports = router;
+
+
+
 const express = require("express");
-const { createReminder } = require("../controllers/reminderController");
-const { protect } = require("../middlewares/authMiddleware");
-
 const router = express.Router();
+const Reminder = require("../models/Reminder");
 
-router.post("/create", protect, createReminder);
+// Set Reminder (Admin Only)
+router.post("/", async (req, res) => {
+  const { role, medicineName, time, dosage, notes, reminderMethod, createdBy } =
+    req.body;
+
+  if (role !== "admin") {
+    return res.status(403).json({ message: "Only admins can set reminders" });
+  }
+
+  const newReminder = new Reminder({
+    medicineName,
+    time,
+    dosage,
+    notes,
+    reminderMethod,
+    createdBy,
+  });
+
+  try {
+    const saved = await newReminder.save();
+    res.status(201).json(saved);
+  } catch (err) {
+    res.status(500).json({ message: "Server error", error: err });
+  }
+});
 
 module.exports = router;
